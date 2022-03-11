df9892254cbab3b1e2749528f879acd3c9d78b37
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-11 16:02:08 +0530

    Added: welcome message

---
+++ src/main/java/com/blz/mood/analyzer/MoodAnalyzer.java
@@ -0,0 +1,5 @@
+package com.blz.mood.analyzer;
+
+public class MoodAnalyzer {
+
+}
---
+++ src/test/java/com/blz/mood/analyzer/MoodAnalyzerTest.java
@@ -0,0 +1,7 @@
+package com.blz.mood.analyzer;
+
+public class MoodAnalyzerTest {
+	public static void main(String[] args) {
+		System.out.println("Welcome to mood analyzer problem");
+	}
+}
