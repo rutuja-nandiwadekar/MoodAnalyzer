0f4a961a0bf6f4052b24fa07a2f1a064b2f589d1
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-14 20:44:45 +0530

    Added: message in constructor logic

--- src/test/java/com/blz/mood/analyzer/MoodAnalyzerTest.java
+++ src/test/java/com/blz/mood/analyzer/MoodAnalyzerTest.java
@@ -7,8 +7,15 @@ public class MoodAnalyzerTest {
 
 	@Test
 	public void givenMessage_SadMood_ShouldReturnSad() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In Sad Mood");
 		String mood = moodAnalyzer.analyseMood();
 		Assert.assertEquals("SAD", mood);
+	}
+
+	@Test
+	public void givenMessage_HappyMood_ShouldReturnHappy() {
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Happy Mood");
+		String mood = moodAnalyser.analyseMood();
+		Assert.assertEquals("HAPPY", mood);
 	}
 }
