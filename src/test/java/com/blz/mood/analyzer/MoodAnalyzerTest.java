fc262cf63bb87c20d7b4c137439d808226809d92
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-14 20:57:26 +0530

    Refactor: to take the mood message in constructor logic

--- src/test/java/com/blz/mood/analyzer/MoodAnalyzerTest.java
+++ src/test/java/com/blz/mood/analyzer/MoodAnalyzerTest.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 public class MoodAnalyzerTest {
 
 	@Test
-	public void givenMessage_SadMood_ShouldReturnSad() {
+	public void givenMessage_SadMood_ShouldReturnsSad() {
 		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In Sad Mood");
 		String mood = moodAnalyzer.analyseMood();
 		Assert.assertEquals("SAD", mood);
 	}
 
 	@Test
-	public void givenMessage_HappyMood_ShouldReturnHappy() {
+	public void givenMessage_HappyMood_ShouldReturnsHappy() {
 		MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Happy Mood");
 		String mood = moodAnalyser.analyseMood();
 		Assert.assertEquals("HAPPY", mood);
